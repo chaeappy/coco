b6e71969d3525b2fb17f8dca20042f310be78d7c
chaeappy <cheappy@gmail.com>
2021-01-14 13:35:02 +0900

    서버 연결 안되는 이유는 'SpringConfig' 파일의 5째줄 때문이었음 --> 삭제 후 정상작동

--- src/main/java/com/cafe/coco/repository/JdbcMemberRepository.java
+++ src/main/java/com/cafe/coco/repository/JdbcMemberRepository.java
@@ -1,9 +1,10 @@
 package com.cafe.coco.repository;
 
 import com.cafe.coco.domain.Member;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 
 import javax.sql.DataSource;
-import java.sql.*;
+import java.sql.Connection;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,64 +14,15 @@ public class JdbcMemberRepository implements MemberRepository{
 
     public JdbcMemberRepository(DataSource dataSource) {
         this.dataSource = dataSource;
-
     }
 
     @Override
     public Member save(Member member) {
-        String sql = "insert into customer values(?, ?)";
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try {
-            conn = dataSource.getConnection();
-            ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, member.getId());
-            ps.setString(2, member.getPw());
-            ps.executeUpdate();
-            rs = ps.getGeneratedKeys();
-
-            if (rs.next()) {
-                member.setNum(rs.getLong(1));
-            } else {
-                throw new SQLException("실패");
-            }
-            return member;
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        } finally {
-            close(conn, ps, rs);
-        }
-
+        return null;
     }
 
     @Override
     public Optional<Member> findByNum(Long num) {
-        String sql = "select * from customer where email = (?)";
-        Connection conn;
-        PreparedStatement ps;
-        ResultSet rs;
-
-        try {
-            conn = dataSource.getConnection();
-            ps = conn.prepareStatement(sql);
-            ps.setLong(1, num);
-            rs = ps.executeQuery();
-
-            if (rs.next()) {
-                Member member = new Member();
-                member.setNum(rs.getLong("num"));
-                member.setId(rs.getString("id"));
-                member.setPw(rs.getString("pw"));
-                return Optional.of(member);
-            } else {
-                return Optional.empty();
-            }
-
-        } catch (Exception e) {
-
-        }
         return Optional.empty();
     }
 
@@ -89,33 +41,8 @@ public class JdbcMemberRepository implements MemberRepository{
         return null;
     }
 
-//    private Connection getConnection() {
-//        return DataSourceUtils.getConnection(dataSource);
-//    }
-
-    private void close(Connection conn, PreparedStatement ps, ResultSet rs) {
-        try {
-            if (conn != null) {
-                rs.close();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            if (ps != null) {
-                ps.close();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            if (rs != null) {
-                rs.close();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    private Connection getConnection() {
+        return DataSourceUtils.getConnection(dataSource);
     }
+
 }
