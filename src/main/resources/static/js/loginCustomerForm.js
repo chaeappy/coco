f64c5d5bd4b99862584b7ee8e298ac42d2d03845
chaeappy <cheappy@gmail.com>
2021-01-15 19:07:03 +0900

    회원가입 구현완료

---
+++ src/main/resources/static/js/loginCustomerForm.js
@@ -0,0 +1,12 @@
+function popup() {
+    alert("로그인 실패");
+}
+
+window.onload=function() {
+    document.getElementById('loginBtn').onclick = function() {
+        var id = document.login.id;
+        var StringId = id.value;
+        if ()
+        popup();
+    }
+}
