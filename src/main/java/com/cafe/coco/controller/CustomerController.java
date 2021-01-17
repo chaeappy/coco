a97d481942671298a4fec8e0f6916d65b7c84c4c
chaeappy <cheappy@gmail.com>
2021-01-17 19:48:42 +0900

    (오류) 로그인페이지 기능 구현까지 완료 --> 자바스크립트 코드에서 location.href로 /orders 페이지로 이동 못하는 이유가 뭘까?

--- src/main/java/com/cafe/coco/controller/CustomerController.java
+++ src/main/java/com/cafe/coco/controller/CustomerController.java
@@ -82,8 +82,9 @@ public class CustomerController {
     @PostMapping("/customers/login")
     public String login(@RequestBody String info) throws ParseException {
         String[] strArr = info.split(",");
+        System.out.println("");
+        System.out.println("");
         Customer customer = customerService.findOne(strArr[0], strArr[1]);
-        System.out.println(customer.getId());
         if (customer != null) {
             return "0";
         } else {
